02506b198f0d08d82b19ebf802952a08c2b0e03c
Eloise Nash <e.nash@v0idx.com>
2026-09-15 03:14:33 +0100

    Update package name Modify binary target to maintain simple cli usage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{Args, Parser, Subcommand, ValueEnum};
-use woodpecker::TodoList;
+use woodpecker_todo::TodoList;
 
 #[derive(Parser)]
 #[command(version, about, long_about=None)] // Read values from Cargo.toml
